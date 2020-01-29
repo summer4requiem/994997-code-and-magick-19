3d4e138f9c9e1a6e91d681a7e2ff03d4b0776f02
summer4requiem <summer.requiem96@gmail.com>
2020-01-29 22:06:48 +0300

    added template part2

--- js/setup.js
+++ js/setup.js
@@ -5,6 +5,7 @@ var WIZARD_SURNAME = ['да Марья', 'Верон', 'Мирабелла', 'В
 var EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green', 'rgb(56, 159, 117)'];
 var COAT_COLORS = ['rgba(101, 137, 164)', 'rgba(241, 43, 107)', 'rgba(146, 100, 161)', 'rgba(56, 159, 117)', 'rgba(215, 210, 55)', 'rgba(0, 0, 0)'];
 var listSimilarWizard = document.querySelector('.setup-similar-list');
+var wizardsArray = [];
 
 document.querySelector('.setup-similar').classList.remove('hidden');
 document.querySelector('.setup').classList.remove('hidden');
@@ -13,31 +14,29 @@ var similarWizardTemplate = document.querySelector('#similar-wizard-template').c
 
 
 var getRandomData = function (array) {
-  return [Math.floor(Math.random() * array.length)];
+  return array[Math.floor(array) * array.length];
 };
 
-
 var createNewWizzards = function (length) {
-
-  var wizardsArray = [];
   for (var i = 0; i < length; i++) {
     wizardsArray.push({
-      name: WIZARD_NAMES[getRandomData] + '' + WIZARD_SURNAME[getRandomData],
-      coatColor: COAT_COLORS[getRandomData],
-      eyeColor: EYES_COLOR[getRandomData],
+      name: getRandomData(WIZARD_NAMES) + '' + getRandomData(WIZARD_SURNAME),
+      coatColor: getRandomData(COAT_COLORS),
+      eyeColor: getRandomData(EYES_COLOR),
     });
   }
   return wizardsArray;
 };
 
 var wizards = createNewWizzards(4);
+// eslint-disable-next-line no-console
+console.log(wizards);
 
 var drawWizards = function (wizard) {
   var wizardHeros = similarWizardTemplate.cloneNode(true);
   wizardHeros.querySelector('.setup-similar-label').textContent = wizard.name;
   wizardHeros.querySelector('.wizard-coat').style.fill = wizard.coatColor;
   wizardHeros.querySelector('.wizard-eyes').style.fill = wizard.eyeColor;
-
   return wizardHeros;
 };
 
