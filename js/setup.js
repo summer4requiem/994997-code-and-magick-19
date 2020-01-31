e3711b8294f178e432abce8b0ce10606de27b21f
summer4requiem <summer.requiem96@gmail.com>
2020-01-31 10:42:49 +0300

    added template part4

--- js/setup.js
+++ js/setup.js
@@ -2,8 +2,10 @@
 
 var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARD_SURNAME = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-var EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green'];
-var COAT_COLORS = ['rgba(101, 137, 164)', 'rgba(241, 43, 107)', 'rgba(146, 100, 161)', 'rgba(56, 159, 117)', 'rgba(215, 210, 55)', 'rgba(215, 260, 55)'];
+var EYES_COLOR = ['black', 'red', 'blue', 'yellow', 'green', 'lightblue'];
+var COAT_COLORS = ['rgba(0, 191, 255)', 'rgba(181, 48, 255)', 'rgba(47, 213, 137)', 'rgba(255, 0, 102)', 'rgba(215, 210, 55)'];
+var MaxWizardsAmount = 4;
+
 // Персонажи
 var listSimilarWizard = document.querySelector('.setup-similar-list');
 
@@ -15,8 +17,7 @@ var similarWizardTemplate = document.querySelector('#similar-wizard-template').c
 
 
 var getRandomData = function (array) {
-  return Math.floor(Math.random() * Math.floor(array - 1));
-
+  return Math.floor(Math.random() * array.length);
 };
 
 // заполнение массива  персонажами
@@ -25,26 +26,26 @@ var wizardsArray = [];
 var createNewWizzards = function (length) {
   for (var i = 0; i < length; i++) {
     wizardsArray.push({
-      name: WIZARD_NAMES[getRandomData(WIZARD_NAMES.length)] + ' ' + WIZARD_NAMES[getRandomData(WIZARD_SURNAME.length)],
-      coatColor: COAT_COLORS[getRandomData(COAT_COLORS.length)],
-      eyesColor: EYES_COLOR[getRandomData(EYES_COLOR.length)]
+      name: WIZARD_NAMES[getRandomData(WIZARD_NAMES)] + ' ' + WIZARD_SURNAME[getRandomData(WIZARD_SURNAME)],
+      coatColor: COAT_COLORS[getRandomData(COAT_COLORS)],
+      eyesColor: EYES_COLOR[getRandomData(EYES_COLOR)],
     });
   }
 };
 
-createNewWizzards(wizardsArray);
+createNewWizzards(MaxWizardsAmount);
 
 
 var drawWizards = function (wizard) {
   var wizardHeros = similarWizardTemplate.cloneNode(true);
   wizardHeros.querySelector('.setup-similar-label').textContent = wizard.name;
   wizardHeros.querySelector('.wizard-coat').style.fill = wizard.coatColor;
-  wizardHeros.querySelector('.wizard-eyes').style.fill = wizard.eyeColor;
+  wizardHeros.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
   return wizardHeros;
 };
 
 var fragment = document.createDocumentFragment();
-for (var i = 0; i < wizardsArray; i++) {
+for (var i = 0; i < wizardsArray.length; i++) {
   fragment.appendChild(drawWizards(wizardsArray[i]));
 }
 
